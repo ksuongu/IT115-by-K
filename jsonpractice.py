22f424d410c82d019ff16a25615040fdcaa6375e
ksuongu <ksuong@sea.yearup.org>
2024-05-15 00:56:32 -0700

    First Commit

---
+++ jsonpractice.py
@@ -0,0 +1,11 @@
+#import of json class library
+import json
+
+#create data dictionary
+data = {
+    'name'= 'Theo Molin'
+    'age' = 28
+    'city'= 'Revere,MA'
+    'interests'= ['Zouking', 'Bachata', 'Argentine Tango', 'BBoying', 'Choreo']
+    'is_student' = False
+}
